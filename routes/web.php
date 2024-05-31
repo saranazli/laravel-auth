895357b62976d73b984b010849bf02bdfeb82473
s4r44d43 <sararez@live.it>
2024-05-31 16:43:54 +0200

    [add]

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\http\Controllers\guest\PageController;
@@ -17,9 +18,12 @@ use App\http\Controllers\guest\PageController;
 
 Route::get('/',[PageController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('home');
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
